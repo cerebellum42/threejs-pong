e6145c023d47191586979cc6ed9804f19bf97ffd
Stefan Kaufhold <cere@fastmail.fm>
2015-11-30 15:20:45 +0100

    game class annotations

--- js/game.js
+++ js/game.js
@@ -5,18 +5,14 @@ define(['behaviour', 'ball', 'play-area', 'rotatable-camera', 'paddle'], functio
         create: function () {
             Behaviour.prototype.create.apply(this, arguments);
 
-            this.width = 1200;
-            this.height = 600;
             this.canvasWrapper = document.getElementById('game-canvas');
         },
         start: function () {
-            this.renderer = new THREE.WebGLRenderer();
-            this.renderer.setSize(this.width, this.height);
-            this.canvasWrapper.appendChild(this.renderer.domElement);
+            // TODO: Three js renderer erstellen und der Seite hinzufügen
 
             this.scene = new THREE.Scene();
 
-
+            this.addLights();
             this.cameraScript = this.addChild(
                 new RotatableCamera({
                     aspectRatio: this.width / this.height,
@@ -26,10 +22,9 @@ define(['behaviour', 'ball', 'play-area', 'rotatable-camera', 'paddle'], functio
                 })
             );
 
-            this.addLights();
-
             var fieldWidth = 180;
             var fieldHeight = 250;
+            var paddleSpeed = 10;
 
             var plane = this.addChild(
                 new PlayArea({
@@ -38,8 +33,6 @@ define(['behaviour', 'ball', 'play-area', 'rotatable-camera', 'paddle'], functio
                     height: fieldHeight
                 })
             );
-
-            var paddleSpeed = 10;
 
             var paddle1 = this.addChild(
                 new Paddle({
@@ -80,39 +73,25 @@ define(['behaviour', 'ball', 'play-area', 'rotatable-camera', 'paddle'], functio
                 })
             );
 
-            this.clock = new THREE.Clock(true);
-            this.delta = 0;
-            this.update(this.delta);
+            this.update(0);
         },
         /**
          * Add lights to the scene
          */
         addLights: function () {
-            var mainLight = new THREE.PointLight(0xF8D898);
-            mainLight.position.x = -1000;
-            mainLight.position.y = 0;
-            mainLight.position.z = 1000;
-            mainLight.intensity = 3;
-            mainLight.distance = 5000;
-            this.scene.add(mainLight);
-
-            var offLight = new THREE.PointLight(0xF8D898);
-            offLight.position.x = 1000;
-            offLight.position.y = 0;
-            offLight.position.z = 1000;
-            offLight.intensity = 1;
-            offLight.distance = 5000;
-            this.scene.add(offLight);
+            // TODO: zwei Lichter mit three js erstellen, um die Szene auszuleuchten
         },
         /**
          * Calls itself every frame through requestAnimationFrame and manages passing and calculating
          * the time delta to its child objects
          */
         update: function () {
-            this.delta = this.clock.getDelta();
-            Behaviour.prototype.update.apply(this, [this.delta]);
-            this.renderer.render(this.scene, this.cameraScript.camera);
-            requestAnimationFrame(this.update.bind(this));
+            /**
+             * TODO:
+             * game loop implementieren
+             * Zeit seit dem letzten Aufruf berechnen (delta)
+             * update(delta) der übergeordneten Klasse (Behaviour) aufrufen
+             */
         }
     });
 
